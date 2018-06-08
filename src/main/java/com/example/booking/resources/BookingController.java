b124ec25d447c569869469cda6d8cdf872d28f76
nalluri <Narendra_Alluri@intuit.com>
2018-06-08 01:11:05 -0500

    API Client

--- src/main/java/com/example/booking/resources/BookingController.java
+++ src/main/java/com/example/booking/resources/BookingController.java
@@ -22,8 +22,6 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-
 @RestController
 @RequestMapping(value = "/bookings", produces = MediaTypes.ALPS_JSON_VALUE)
 public class BookingController {
@@ -48,10 +46,6 @@ public class BookingController {
         List<Booking> bookings = passengerUUID.map(uuid -> convertEntityToDTO(
                 bookingService.getBookingsForPassenger(passengerUUID.get()))).orElse(Collections.emptyList());
 
-        //TODO:hateoas
-        //Resources<Booking> resources = new Resources<>(bookings);
-        //addLinks(resources, passengerId);
-
         return new ResponseEntity<>(bookings, new HttpHeaders(), HttpStatus.OK);
 
 
@@ -63,45 +57,18 @@ public class BookingController {
         Optional<UUID> bookingUUID = getUUID(bookingId);
         if (!bookingUUID.isPresent()) {
             BookingDetails bookingDetails = new BookingDetails();
-            //addLinks(bookingDetails, bookingId);//TODO:hateoas
             return new ResponseEntity<>(bookingDetails, HttpStatus.BAD_REQUEST);
         }
         Optional<com.example.booking.entities.Booking> bookingEntity = bookingService.getBookingDetails(bookingUUID.get());
 
 
         BookingDetails bookingDetails = bookingEntity.map(booking -> modelMapper.map(booking, BookingDetails.class))
-                .orElse(new BookingDetails());
+                .orElse(null);
 
         HttpStatus status = bookingEntity.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND;
 
-        //addLinks(bookingDetails, bookingId); //TODO:hateoas
-
         return new ResponseEntity<>(bookingDetails, new HttpHeaders(), status);
     }
-
-// TODO:hateoas
-//    private void addLinks(BookingDetails bookingDetails, String bookingId) {
-//        bookingDetails.add(
-//                linkTo(methodOn(BookingController.class)
-//                        .getBookingDetails(bookingId))
-//                        .withSelfRel());
-//    }
-//
-//    private void addLinks(Resources<Booking> resources, String passengerId) {
-//
-//        resources.add(
-//                linkTo(methodOn(BookingController.class)
-//                        .getPassengerBookings(passengerId))
-//                        .withSelfRel());
-//
-//        Optional<Booking> firstBooking = resources.getContent().stream().findFirst();
-//
-//        firstBooking.ifPresent(booking -> resources.add(
-//                linkTo(methodOn(BookingController.class)
-//                        .getBookingDetails(booking.getBookingId()))
-//                        .withRel("bookingDetails")));
-//
-//    }
 
     private List<Booking> convertEntityToDTO(List<com.example.booking.entities.Booking> bookingList) {
         return bookingList.stream()
---
+++ src/test/java/com/example/booking/developertests/BookingClient.java
@@ -0,0 +1,42 @@
+package com.example.booking.developertests;
+
+import com.example.booking.model.Booking;
+import com.example.booking.model.BookingDetails;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class BookingClient {
+
+    private static String BASE_URL = "http://localhost:8080/bookings";
+
+
+    @Test
+    public void testAPI() {
+        RestTemplate restTemplate = new RestTemplate();
+        String resourceURL = String.format("%s?%s", BASE_URL, "passengerId=a57eace4-38bf-4cd9-93d5-936bd584c929");
+        ResponseEntity<List<Booking>> bookingsForPassengerEntity =
+                restTemplate.exchange(resourceURL,
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Booking>>() {
+                        });
+        List<Booking> bookingsForPassenger = bookingsForPassengerEntity.getBody();
+        System.out.println(bookingsForPassenger.size());
+        bookingsForPassenger.stream().forEach(booking -> {
+            String bookingResourceURL = String.format("%s/%s", BASE_URL, booking.getBookingId());
+            ResponseEntity<BookingDetails> bookingResponseEntity = restTemplate.exchange(bookingResourceURL,
+                    HttpMethod.GET, null, BookingDetails.class);
+            BookingDetails bookingDetails = bookingResponseEntity.getBody();
+            System.out.println(bookingDetails.getBookingId());
+
+            ResponseEntity<String> bookingResponseString = restTemplate.exchange(bookingResourceURL,
+                    HttpMethod.GET, null, String.class);
+
+            System.out.println(bookingResponseString.getBody());
+        });
+    }
+}
